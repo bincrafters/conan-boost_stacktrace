a4ce800578978f338c4ee31a888f85867ab1c348
Jerry W <jerrywiltse@gmail.com>
2017-09-25 20:16:16 -0400

    applied latest standards

--- conanfile.py
+++ conanfile.py
@@ -3,28 +3,42 @@ from conans import ConanFile, tools, os
 class BoostStacktraceConan(ConanFile):
     name = "Boost.Stacktrace"
     version = "1.65.1"
-    generators = "txt" 
+    generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
-    url = "https://github.com/boostorg/stacktrace"
+    url = "https://github.com/bincrafters/conan-boost-stacktrace"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "stacktrace"
-    build_requires = "Boost.Build/1.65.1@bincrafters/testing" 
-    requires =  "Boost.Array/1.65.1@bincrafters/testing", \
-                      "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Core/1.65.1@bincrafters/testing", \
-                      "Boost.Lexical_Cast/1.65.1@bincrafters/testing", \
-                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-                      "Boost.Winapi/1.65.1@bincrafters/testing"
+    lib_short_names = ["stacktrace"]
+    build_requires = "Boost.Generator/1.65.1@bincrafters/stable" 
+    requires =  "Boost.Array/1.65.1@bincrafters/stable", \
+                      "Boost.Config/1.65.1@bincrafters/stable", \
+                      "Boost.Core/1.65.1@bincrafters/stable", \
+                      "Boost.Lexical_Cast/1.65.1@bincrafters/stable", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/stable", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/stable", \
+                      "Boost.Winapi/1.65.1@bincrafters/stable"
 
                       #array3 config0 core2 lexical_cast8 static_assert1 type_traits3 winapi1
 
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
+        for lib_short_name in self.lib_short_names:
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
+    def build(self):
+        self.run(self.deps_user_info['Boost.Generator'].b2_command)
+        
     def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="include", src=include_dir)
+        self.copy(pattern="*", dst="lib", src="stage/lib")
+        for lib_short_name in self.lib_short_names:
+            include_dir = os.path.join(lib_short_name, "include")
+            self.copy(pattern="*", dst="include", src=include_dir)
+
+    def package_info(self):
+        self.user_info.lib_short_names = ",".join(self.lib_short_names)
+        self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")
 
