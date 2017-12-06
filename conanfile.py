0506e7a8806311e9210533ad943b560183a1f5b8
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:49 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,10 @@ from conans import ConanFile, tools
 class BoostStacktraceConan(ConanFile):
     name = "Boost.Stacktrace"
     version = "1.65.1"
-    url = "https://github.com/bincrafters/conan-boost-stacktrace"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
-    license = "www.boost.org/users/license.html"
 
-    settings = "os", "arch", "compiler", "build_type"
 
     requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
         "Boost.Array/1.65.1@bincrafters/testing", \
         "Boost.Config/1.65.1@bincrafters/testing", \
         "Boost.Core/1.65.1@bincrafters/testing", \
@@ -24,22 +21,19 @@ class BoostStacktraceConan(ConanFile):
 
     # BEGIN
 
+    url = "https://github.com/bincrafters/conan-boost-stacktrace"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
     generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
-        # TODO: Can we move this to package_info?
-        self.cpp_info.defines.append("BOOST_STACKTRACE_GNU_SOURCE_NOT_REQUIRED=1")
-
-    # pylint: disable=unused-import
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass
