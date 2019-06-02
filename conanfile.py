96f0683d196bd3ecfe2bcea9315ce2be644e0988
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,24 +4,14 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostStacktraceConan(base.BoostBaseConan):
     name = "boost_stacktrace"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_stacktrace"
-    lib_short_names = ["stacktrace"]
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    b2_requires = [
-        "boost_array",
-        "boost_config",
-        "boost_container_hash",
-        "boost_core",
-        "boost_static_assert",
-        "boost_type_traits",
-        "boost_winapi"
-    ]
+    version = "1.70.0"
 
-    def package_info_additional(self):
-        self.cpp_info.defines.append("BOOST_STACKTRACE_GNU_SOURCE_NOT_REQUIRED=1")
+    def package_info(self):
+        super(BoostStacktraceConan, self).package_info()
+        self.cpp_info.defines.append(
+            "BOOST_STACKTRACE_GNU_SOURCE_NOT_REQUIRED=1")
